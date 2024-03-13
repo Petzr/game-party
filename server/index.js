247cdb6aa086cd4e58f580eae20a2b6660fb167a
Petzr <peterjansen812@gmail.com>
2024-03-13 19:58:10 +0100

    added socket to server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
 const db = require('./database.js');
 const md5 = require('md5')
+
+const express = require('express')
+const { createServer } = require('node:http')
+const { Server } = require('socket.io')
+
 
 // var bodyParser = require("body-parser");
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,26 +13,33 @@ const md5 = require('md5')
 
 const HTTP_PORT = 5000;
 
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
 // Root endpoint
 app.get("/", (req, res) => {
-    res.json({"message":"Ok"})
+    res.json({ "message": "Ok" })
 });
 
+io.on('connection', (socket) => {
+    console.log('user ' + socket.id + ' connected');
+});
 
 app.get("/api/users", (req, res, next) => {
     var sql = "select * from user"
     var params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
 
         res.json({
-            "message":"success",
-            "data":rows
+            "message": "success",
+            "data": rows
         })
-      });
+    });
 });
 
 app.get("/api/user/:id", (req, res, next) => {
@@ -37,14 +47,14 @@ app.get("/api/user/:id", (req, res, next) => {
     var params = [req.params.id]
     db.get(sql, params, (err, row) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
-            "message":"success",
-            "data":row
+            "message": "success",
+            "data": row
         })
-      });
+    });
 });
 
 app.get('/api', (req, res) => {
@@ -60,8 +70,10 @@ app.get('/api', (req, res) => {
 })
 
 // Default response for any other request
-app.use(function(req, res){
+app.use(function (req, res) {
     res.status(404);
 });
 
-app.listen(HTTP_PORT, () => { console.log('Server is listening to port 5000') })
+server.listen(HTTP_PORT, () => {
+    console.log('Server running at http://localhost:' + HTTP_PORT);
+});
